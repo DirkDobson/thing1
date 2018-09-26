610578600d900b597cd5d1ef7773bf0af3f81764
Dirk Dobson <dirkvancouver@live.com>
2018-09-26 11:29:47 -0600

    tweets controller

--- app/controllers/api/tweets_controller.rb
+++ app/controllers/api/tweets_controller.rb
@@ -2,4 +2,8 @@ class Api::TweetsController < ApplicationController
   def index
     render json: TwitterClient.timeline
   end
+
+  def search
+    render json: TwitterClient.by_user(params[:term])
+  end
 end
